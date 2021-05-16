d69daf9a3a8bf89f97a19b9162512f4d1db9dc17
Matteo <matteo.pipitone93@gmail.com>
2021-05-16 12:13:03 +0200

    implemented acceptace test

--- src/test/java/org/articles/UncleBob/TheBowlingGameKata/BowlingGameTest.java
+++ src/test/java/org/articles/UncleBob/TheBowlingGameKata/BowlingGameTest.java
@@ -1,13 +1,36 @@
 package org.articles.UncleBob.TheBowlingGameKata;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BowlingGameTest {
 
-    @Test
-    void firstTest() {
+    private Game game;
 
+    private class Game {
+        public void roll(int pins){}
+        public Integer score(){
+            return null;
+        }
     }
+
+    @BeforeEach
+    void setUp() {
+        game = new Game();
+    }
+
+    @Test
+    @Disabled
+    void allGameAcceptanceTest() {
+        Integer[] pillsDown = new Integer[]{1 ,4 ,4 ,5 ,6 ,4 ,5 ,5 ,10 ,0 ,1 ,7 ,3 ,6 ,4 ,10 ,2 ,8 ,6};
+        for (Integer integer : pillsDown) {
+            game.roll(integer);
+        }
+        assertEquals(game.score(), 133);
+    }
+
+
 }
