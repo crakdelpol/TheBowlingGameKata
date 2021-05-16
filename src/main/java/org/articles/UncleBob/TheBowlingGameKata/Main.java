d14a9e1832733707efab5aa56c2accb3628ef86e
Matteo <matteo.pipitone93@gmail.com>
2021-05-16 11:50:39 +0200

    first commit

---
+++ src/main/java/org/articles/UncleBob/TheBowlingGameKata/Main.java
@@ -0,0 +1,9 @@
+package org.articles.UncleBob.TheBowlingGameKata;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+
+}
---
+++ src/test/java/org/articles/UncleBob/TheBowlingGameKata/BowlingGameTest.java
@@ -0,0 +1,13 @@
+package org.articles.UncleBob.TheBowlingGameKata;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BowlingGameTest {
+
+    @Test
+    void firstTest() {
+
+    }
+}
